9f56d72343207540d15030c902835aaa5d023f4d
Cameron Sweeney <cp_sweeney@hotmail.com>
2018-06-05 14:15:25 -0700

    Add child Synchronizer stubs

--- djautotask/sync.py
+++ djautotask/sync.py
@@ -1,5 +1,6 @@
 from djautotask import models
 from django.conf import settings
+import atws
 
 
 # Might have to move to utils
@@ -16,5 +17,46 @@ class DjautotaskSettings:
 
 class Synchronizer:
 
-    def __init__(self):
-        pass
+    def __init__(self, full=False, *args, **kwargs):
+        request_settings = DjautotaskSettings().get_settings()
+        self.full = full
+
+
+class AccountSynchronizer(Synchronizer):
+    pass
+
+
+class ProjectSynchronizer(Synchronizer):
+    pass
+
+
+class TicketSynchronizer(Synchronizer):
+    pass
+
+
+class TicketCategorySynchronizer(Synchronizer):
+    pass
+
+
+class TimeEntrySynchronizer(Synchronizer):
+    pass
+
+
+class TicketSecondaryResourceSynchronizer(Synchronizer):
+    pass
+
+
+class ResourceSynchronizer(Synchronizer):
+    pass
+
+
+class DepartmentSynchronizer(Synchronizer):
+    pass
+
+
+class RoleSynchronizer(Synchronizer):
+    pass
+
+
+class OpportunitySynchronizer(Synchronizer):
+    pass
--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'django-braces',
         'django-extensions',
         'retrying',
+        'atws',
     ],
     test_suite='runtests.suite',
     tests_require=[
